ad3cc78bf526a15e0e2f0007e85154df09e4a25b
fabiomuzzu <fabio.muzzu@virgilio.it>
2023-12-06 16:20:11 +0100

    starting excercise

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// Creo un array di oggetti
+let bici = [
+    {
+        nome: 'Mountain Bike',
+        peso: '10',
+    },
+    {
+        nome: 'Bmx',
+        peso: '20',
+    },
+    {
+        nome: 'Graziella',
+        peso: '30',
+    },
+];
