fee53cb699718678b153f477b3b29b2f0bcf84b0
fabiomuzzu <fabio.muzzu@virgilio.it>
2023-12-06 18:12:34 +0100

    added snack1

--- js/script.js
+++ js/script.js
@@ -1,18 +1,27 @@
 // Creo un array di oggetti
 let bici = [
     {
-        nome: 'Mountain Bike',
-        peso: '10',
+        nome: 'Bmx',
+        peso: 20,
     },
     {
-        nome: 'Bmx',
-        peso: '20',
+        nome: 'Mountain Bike',
+        peso: 10,
     },
     {
         nome: 'Graziella',
-        peso: '30',
+        peso: 30,
     },
 ];
 
-const {peso} = bici;
-console.log(peso); 
+let pesi = [];
+for(i=0; i<bici.length; i++){
+    pesi.push(bici[i].peso);
+}
+
+const peso_min = Math.min(...pesi);
+
+console.log(peso_min);
+
+console.log(`La bici ${bici[1].nome} è la più leggera con un peso di ${peso_min}`);
+
