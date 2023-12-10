2c0623d9719889a4b57f5d468cfaa7a30c34637e
fabiomuzzu <fabio.muzzu@virgilio.it>
2023-12-10 13:05:32 +0100

    completed excercise

--- js/script.js
+++ js/script.js
@@ -27,3 +27,58 @@ bici.forEach(element => {
 let {nome, peso} = bici_leggera;
 
 console.log(`La bici più leggerà è ${nome} ed ha un peso di ${peso}kg`);
+
+// Snack 2
+
+let squads = [
+    {
+        nome: 'Inter',
+        punti_fatti: 0,
+        falli_subiti: 0,
+    },
+    {
+        nome: 'Milan',
+        punti_fatti: 0,
+        falli_subiti: 0,
+    },
+    {
+        nome: 'Juventus',
+        punti_fatti: 0,
+        falli_subiti: 0,
+    },
+    {
+        nome: 'Napoli',
+        punti_fatti: 0,
+        falli_subiti: 0,
+    },
+    {
+        nome: 'Roma',
+        punti_fatti: 0,
+        falli_subiti: 0,
+    }
+];
+
+// funzione per calcolare numero random tra un valore min e uno max
+function rndNumber(min, max){
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+squads.forEach((elem) => {
+    elem.punti_fatti = rndNumber(10, 100);
+    elem.falli_subiti = rndNumber(30, 80);
+});
+
+let newSquads = [];
+
+squads.forEach((elem) => {
+    let {nome, falli_subiti} = elem;
+
+    let squad = {
+        nome,
+        falli_subiti,
+    }
+
+    newSquads.push(squad);
+})
+
+console.log(newSquads);
